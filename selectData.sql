13d823c7868d1a13cead5a53a8196fbd78a31926
carolineF <1138483832@qq.com>
2015-08-17 17:11:19 +0800

    add selectData from 20 to 30

---
+++ selectData.sql
@@ -0,0 +1,16 @@
+SELECT * FROM student LIMIT 20, 10;
+
+SELECT * FROM class LIMIT 20, 10;
+
+SELECT * FROM course LIMIT 20, 10;
+
+SELECT * FROM day_course LIMIT 20, 10;
+
+SELECT * FROM schedule LIMIT 20, 10;
+
+SELECT * FROM student_course LIMIT 20, 10;
+
+
+SELECT name, course_name, course_time FROM (SELECT course_id , name FROM
+  (SELECT id, name FROM student LIMIT 20, 10) s , student_course
+ WHERE s.id = student_course.student_id) sc, course WHERE course.id = sc.course_id;
