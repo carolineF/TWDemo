47385e796d96c8210b39eaf29e810c7969afcc34
carolineF <1138483832@qq.com>
2015-08-17 21:54:11 +0800

    add modify sex sql

--- selectCount.sql
+++ selectCount.sql
@@ -1,6 +1,6 @@
 SELECT class_name, COUNT(*) FROM
-  (SELECT class_name FROM (SELECT * FROM class) c,student
-  WHERE c.id = student.class_id) s group by s.class_name ;
+  (SELECT class_name FROM class,student
+  WHERE class.id = student.class_id) s group by s.class_name ;
 
 SELECT class_name, sex, COUNT(*) FROM
   (SELECT class_name, sex FROM class,student
